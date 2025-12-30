a458c940021b82faebd6d3c0cdbd5f45e687dc82
Ofek Pintok <OfekPintok@Gmail.com>
2025-12-30 08:14:22 +0200

    implemented CalculateTripsUseCase.kt and CalculateTripsUseCaseTest.kt

--- app/src/main/java/com/ofekpintok/janitor/features/janitor/domain/CalculateTripsUseCase.kt
+++ app/src/main/java/com/ofekpintok/janitor/features/janitor/domain/CalculateTripsUseCase.kt
@@ -1,4 +1,50 @@
 package com.ofekpintok.janitor.features.janitor.domain
 
+import com.ofekpintok.janitor.features.janitor.domain.model.Bag
+import com.ofekpintok.janitor.features.janitor.domain.model.Trip
+
 class CalculateTripsUseCase {
+
+    operator fun invoke(bags: List<Bag>): List<Trip> {
+        val sortedBagsList = bags.sortedBy { it.weight }
+
+        var right = bags.size - 1
+        var left = 0
+
+        return mutableListOf<Trip>().apply {
+            while (right >= left) {
+                val rightBag = sortedBagsList[right]
+                val leftBag = sortedBagsList[left]
+
+                if (right == left) {
+                    add(
+                        Trip(
+                            id = size,
+                            bags = listOf(rightBag)
+                        )
+                    )
+                    break
+                }
+
+                if (rightBag.weight + leftBag.weight > 3) {
+                    add(
+                        Trip(
+                            id = size,
+                            bags = listOf(rightBag)
+                        )
+                    )
+                    right--
+                } else {
+                    add(
+                        Trip(
+                            id = size,
+                            bags = listOf(rightBag, leftBag)
+                        )
+                    )
+                    right--
+                    left++
+                }
+            }
+        }
+    }
 }
--- app/src/test/java/com/ofekpintok/janitor/CalculateTripsUseCaseTest.kt
+++ app/src/test/java/com/ofekpintok/janitor/CalculateTripsUseCaseTest.kt
@@ -1,4 +1,81 @@
 package com.ofekpintok.janitor
 
+import com.ofekpintok.janitor.features.janitor.domain.CalculateTripsUseCase
+import com.ofekpintok.janitor.features.janitor.domain.model.Bag
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class CalculateTripsUseCaseTest {
+    val sut = CalculateTripsUseCase()
+
+    @Test
+    fun calculateTripZeroBags() {
+        assertEquals(
+            0,
+            sut(bags = emptyList()).size
+        )
+    }
+
+    @Test
+    fun calculateTripSingleBag() {
+        assertEquals(
+            1,
+            sut(bags = listOf(Bag(0, 2.0))).size
+        )
+    }
+
+    @Test
+    fun calculateTripAllBagsAreOverweight() {
+        assertEquals(
+            3,
+            sut(
+                bags = listOf(
+                    Bag(0, 2.0),
+                    Bag(1, 3.0),
+                    Bag(2, 2.5)
+                )
+            ).size
+        )
+    }
+
+    @Test
+    fun calculateTripOddList() {
+        assertEquals(
+            2,
+            sut(
+                bags = listOf(
+                    Bag(0, 1.99),
+                    Bag(1, 1.01),
+                    Bag(2, 2.5)
+                )
+            ).size
+        )
+    }
+
+
+    @Test
+    fun calculateTripEvenList() {
+        assertEquals(
+            2,
+            sut(
+                bags = listOf(
+                    Bag(1, 1.01),
+                    Bag(2, 2.5)
+                )
+            ).size
+        )
+    }
+
+    @Test
+    fun calculateTripExactLimit() {
+        assertEquals(
+            1,
+            sut(
+                bags = listOf(
+                    Bag(1, 1.5),
+                    Bag(2, 1.5)
+                )
+            ).size
+        )
+    }
 }
